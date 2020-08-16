f20b97510f9a5b72822947eb64f2d75a2ed173e6
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2020-08-16 19:48:57 +0000

    update css

--- lib/simp_web/live/modal_component.ex
+++ lib/simp_web/live/modal_component.ex
@@ -4,15 +4,15 @@ defmodule SimpWeb.ModalComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <div id="<%= @id %>" class="phx-modal"
+    <div id="<%= @id %>" class="modal"
       phx-capture-click="close"
       phx-window-keydown="close"
       phx-key="escape"
       phx-target="#<%= @id %>"
       phx-page-loading>
 
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
+      <div class="modal__content">
+        <%= live_patch raw("&times;"), to: @return_to, class: "modal__close" %>
         <%= live_component @socket, @component, @opts %>
       </div>
     </div>
