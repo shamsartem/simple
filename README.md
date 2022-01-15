f2573fd471c169ed2ad3b4ecb2e428bbf1cbd84e
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2022-01-15 12:55:34 +0300

    Add simple calculator to price and amount

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-# Docker Phoenix
+# Simple. Uses Docker Phoenix
 
 ## Prerequisites
+
 Install docker and docker-compose.
 Duplicate `.example.env` file and rename it to `.env`
 
 ## Motivation
+
 I wanted to have a starter kit for Phoenix projects that I would be able to run anywhere in docker.
 Alpine linux is used for all the docker images.
 If you use vscode - all the necessary extensions for developing phoenix application will be installed automatically inside the container
 
 ## Set up for vscode users
+
 1. Install https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.vscode-remote-extensionpack
 2. Open the folder in vscode and it should suggest you to reopen it inside the container
 3. After setting up the database you can add connection in postresql explorer (use hostname `db`, user and password from `.env` file)
 4. In `devcontainer.json` set `elixir.projectPath` and `elixirLS.projectDir` to the path to your project so elixir-ls extension starts working.
 
 ## Get started
+
 1. Start shell session inside the container. For vscode users - just open the terminal (Ctrl + J) - it will already be inside the container. For others - run e.g. `docker exec -it phoenix-docker_app_1 sh`
 2. Create new phoenix project `mix phx.new your_project_name` and press enter to install dependencies when it prompts you
 3. To set up postgres open `config/dev.exs`, change hostname to `"db"`, database to `"database"` and run `mix ecto.create` in your project folder
--- lib/simp_web/live/transaction_live/form_component.ex
+++ lib/simp_web/live/transaction_live/form_component.ex
@@ -11,6 +11,7 @@ defmodule SimpWeb.TransactionLive.FormComponent do
      socket
      |> assign(assigns)
      |> assign(:changeset, changeset)
+     |> assign(:previous_is_expense, Ecto.Changeset.get_field(changeset, :is_expense))
      |> set_data()}
   end
 
@@ -18,15 +19,25 @@ defmodule SimpWeb.TransactionLive.FormComponent do
          %{
            assigns: %{
              current_user: current_user,
-             changeset: changeset
+             changeset: changeset,
+             previous_is_expense: previous_is_expense
            }
          } = socket
        ) do
+    is_expense = Ecto.Changeset.get_field(changeset, :is_expense)
+
     categories =
       Transactions.list_categories(
         current_user,
-        Ecto.Changeset.get_field(changeset, :is_expense)
+        is_expense
       )
+
+    changeset =
+      if previous_is_expense == is_expense do
+        changeset
+      else
+        Ecto.Changeset.change(changeset, category: List.first(categories) || "")
+      end
 
     names =
       Transactions.list_names(
@@ -37,12 +48,16 @@ defmodule SimpWeb.TransactionLive.FormComponent do
     assign(socket,
       categories: categories,
       names: names,
-      currencies: Transactions.list_currencies(current_user)
+      currencies: Transactions.list_currencies(current_user),
+      changeset: changeset,
+      previous_is_expense: is_expense
     )
   end
 
   @impl true
   def handle_event("validate", %{"transaction" => transaction_params}, socket) do
+    transaction_params = parse_price_and_amount(transaction_params)
+
     changeset =
       socket.assigns.transaction
       |> Transactions.change_transaction(transaction_params)
@@ -68,6 +83,8 @@ defmodule SimpWeb.TransactionLive.FormComponent do
           }
         } = socket
       ) do
+    transaction_params = parse_price_and_amount(transaction_params)
+
     if current_user.id == transaction.user_id do
       case Transactions.update_transaction(transaction, transaction_params) do
         {:ok, _transaction} ->
@@ -95,6 +112,8 @@ defmodule SimpWeb.TransactionLive.FormComponent do
           }
         } = socket
       ) do
+    transaction_params = parse_price_and_amount(transaction_params)
+
     case Transactions.create_transaction(transaction_params, current_user) do
       {:ok, _transaction} ->
         {:noreply,
@@ -112,6 +131,105 @@ defmodule SimpWeb.TransactionLive.FormComponent do
       ""
     else
       "_new"
+    end
+  end
+
+  def parse_price_and_amount(%{"price" => price, "amount" => amount} = transaction_params) do
+    price_parsed = parse_price_or_amount(price)
+    transaction_params = Map.put(transaction_params, "price", price_parsed)
+
+    amount_parsed = parse_price_or_amount(amount)
+    Map.put(transaction_params, "amount", amount_parsed)
+  end
+
+  def parse_price_or_amount(string) do
+    parsingError = %{
+      hasError: true,
+      isDotUsed: false,
+      sign: nil,
+      num: "",
+      res: 0
+    }
+
+    %{sign: sign, num: num, res: res, isDotUsed: isDotUsed, hasError: hasError} =
+      Enum.reduce(
+        String.graphemes(string),
+        %{sign: "+", num: "", res: 0, isDotUsed: false, hasError: false},
+        fn symbol, %{sign: sign, num: num, res: res, isDotUsed: isDotUsed, hasError: hasError} ->
+          cond do
+            hasError or
+              (Enum.member?(["+", "-"], symbol) and num === "") or
+                ((symbol === "." and isDotUsed) or
+                   (symbol === "." and num === "")) ->
+              parsingError
+
+            Enum.member?(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], symbol) ->
+              %{
+                hasError: false,
+                sign: sign,
+                num: num <> symbol,
+                res: res,
+                isDotUsed: isDotUsed
+              }
+
+            symbol === "." ->
+              %{
+                hasError: false,
+                sign: sign,
+                num: num <> symbol,
+                res: res,
+                isDotUsed: true
+              }
+
+            Enum.member?(["+", "-"], symbol) ->
+              %{
+                hasError: false,
+                sign: symbol,
+                num: "",
+                isDotUsed: false,
+                res:
+                  res +
+                    if sign === "+" do
+                      {parsed, _} = Float.parse(num)
+                      parsed
+                    else
+                      {parsed, _} = Float.parse(num)
+                      -parsed
+                    end
+              }
+
+            true ->
+              parsingError
+          end
+        end
+      )
+
+    cond do
+      hasError ->
+        "error"
+
+      true ->
+        if num === "" do
+          ""
+        else
+          res =
+            res +
+              if sign === "+" do
+                {parsed, _} = Float.parse(num)
+                parsed
+              else
+                {parsed, _} = Float.parse(num)
+                -parsed
+              end
+
+          [integer_part, decimal_part] = String.split(Float.to_string(res), ".")
+
+          if decimal_part == "0" do
+            String.to_integer(integer_part)
+          else
+            res
+          end
+        end
     end
   end
 end
